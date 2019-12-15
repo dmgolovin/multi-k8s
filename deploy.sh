2c0421b234f76d36f00b1fc9333fb3bc17661c77
Dmitry <dm.golovin@yahoo.com>
2019-12-14 21:11:30 -0500

    No dockerhub login

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
-docker build -t dmgolovin/multi-client - f./client/Dockerfile ./client
-docker build -t dmgolovin/multi-server - f./server/Dockerfile ./server
-docker build -t dmgolovin/multi-worker - f./worker/Dockerfile ./worker
-docker push dmgolovin/multi-client 
-docker push dmgolovin/multi-server
-docker push dmgolovin/multi-worker
+docker build -t dgolovin2/multi-client - f./client/Dockerfile ./client
+docker build -t dgolovin2/multi-server - f./server/Dockerfile ./server
+docker build -t dgolovin2/multi-worker - f./worker/Dockerfile ./worker
+docker push dgolovin2/multi-client 
+docker push dgolovin2/multi-server
+docker push dgolovin2/multi-worker
 kubectl apply -f k8s
-kubectl image deployments/server-deployment server=dmgolovin/multi-server:$SHA
-kubectl image deployments/client-deployment server=dmgolovin/multi-client:$SHA
-kubectl image deployments/worker-deployment server=dmgolovin/multi-worker:$SHA
+kubectl image deployments/server-deployment server=dgolovin2/multi-server:$SHA
+kubectl image deployments/client-deployment server=dgolovin2/multi-client:$SHA
+kubectl image deployments/worker-deployment server=dgolovin2/multi-worker:$SHA
 
 
 
